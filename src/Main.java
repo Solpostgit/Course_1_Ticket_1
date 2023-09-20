fb033ae1cf8428e0fcc86c52b3b4eeb810605491
Solpostgit <begemot_off@mail.ru>
2023-09-20 23:31:49 +0300

    Решил задачу 1 билета

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = new int[5];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+        System.out.println("Исходный массив array " + Arrays.toString(array));
+
+        reverseArray(array); //метод реверса массива
+
+    }
+    public static void reverseArray(int[] arr) {
+        int[] arr2 = new int[arr.length];
+        for (int i = 0;i < arr.length; i++){
+            arr2[i] = arr[arr.length-1-i];
+        }
+        System.out.println();
+        System.out.println("Массив arr2 " + Arrays.toString(arr2));
+        arr = Arrays.copyOf(arr2, arr2.length);
+        System.out.println("Массив array " + Arrays.toString(arr));
     }
 }
