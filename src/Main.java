620e19bd22673f18fb574dc95f28755504a6ba91
Solpostgit <begemot_off@mail.ru>
2023-09-20 23:33:11 +0300

    Решил задачу 1 билета, поменял написание массива

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,15 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[5];
-        array[0] = 1;
-        array[1] = 2;
-        array[2] = 3;
-        array[3] = 4;
-        array[4] = 5;
+        int[] array = {1, 2, 3, 4, 5};
         System.out.println("Исходный массив array " + Arrays.toString(array));
-
         reverseArray(array); //метод реверса массива
-
     }
+
     public static void reverseArray(int[] arr) {
         int[] arr2 = new int[arr.length];
-        for (int i = 0;i < arr.length; i++){
-            arr2[i] = arr[arr.length-1-i];
+        for (int i = 0; i < arr.length; i++) {
+            arr2[i] = arr[arr.length - 1 - i];
         }
         System.out.println();
         System.out.println("Массив arr2 " + Arrays.toString(arr2));
